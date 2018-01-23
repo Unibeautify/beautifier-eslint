3a33085e4c000dac591ce2ed1d1e89ddb58d4a85
Steven Zeck <steven.zeck@gmail.com>
2018-01-23 10:17:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# beautifier-eslint
+
+> [ESLint](https://eslint.org) beautifier for [Unibeautify](https://github.com/Unibeautify)
+
+## Installation
+
+```bash
+npm install --global @unibeautify/beautifier-eslint
+```
+
+## Usage
+
+See [`unibeautify-cli`](https://github.com/Unibeautify/unibeautify-cli) for details.
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { Beautifier, Language } from "unibeautify";
+import { wrapBeautifier, AtomPackage } from "unibeautify-beautifier";
+const CLIEngine = require("eslint").CLIEngine;
+
+const pkg = require("../package.json");
+
+export const beautifier: Beautifier = {
+  name: "ESLint",
+  // link: "https://github.com/eslint/eslint",
+  options: {
+    _: {
+      "newline-per-chained-call": [
+        ["break_chained_methods"],
+        function(options) {
+          if (options.break_chained_methods === true) {
+            return 2;
+          } else {
+            return 2;
+          }
+        }
+      ]
+    },
+    JSX: true,
+    JavaScript: true
+  },
+  beautify(data) {
+    return new Promise<string>((resolve, reject) => {
+      const cli = new CLIEngine({
+        fix: true,
+        rules: data.options
+      });
+      const text = cli.executeOnText(data.text).results[0].output;
+      if (text) {
+        return resolve(text);
+      }
+      else {
+        return resolve(data.text);
+      }
+    }) as any;
+  }
+};
+
+export function provideBeautifier() {
+  return beautifier;
+}
+
+const config = {};
+
+const wrappedBeautifier: Beautifier | AtomPackage = wrapBeautifier(
+  pkg,
+  beautifier,
+  config
+);
+export { Beautifier, AtomPackage };
+export default wrappedBeautifier;
